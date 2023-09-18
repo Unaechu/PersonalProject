1c4cfbd08ed55907acdbb39025ee7975d7554b47
yunhye <yhc444@gmail.com>
2023-09-19 00:37:38 +0900

    0918

--- funeral/funeral_facilities.py
+++ funeral/funeral_facilities.py
@@ -1,29 +1,41 @@
 import pymysql
+
 db = pymysql.connect(host="database-1.c96y4v8xbpbh.ap-northeast-2.rds.amazonaws.com",
                      port=3306, user="admin", passwd="zofhfl6655", db="facilities", charset="utf8")
-cursor= db.cursor()
+cursor = db.cursor()
 
 def main():
-    q = """SELECT * FROM funeral"""
-    row_funeral_ids = cursor.execute(q)
+    try:
+        q = """SELECT * FROM funeral"""
+        cursor.execute(q)
+        row_funeral_ids = cursor.fetchall()
 
-    q = """SELECT * FROM funeral_facilities"""
-    row_funeral_details = cursor.execute(q)
+        q = """SELECT * FROM funeral_facilities"""
+        cursor.execute(q)
+        row_funeral_details = cursor.fetchall()
 
-#    q = """SELECT * FROM funeral_info"""
-#    row_funeral_infos = cursor.excute(q)
+#       q = """SELECT * FROM funeral_info"""
+#       row_funeral_infos = cursor.execute(q)
 
-    q = """SELECT * FROM funeral_facilities WHERE 장례식장명 (SELECT 장례식장명 FROM funeral)
-    ALTER TABLE funeral_facilities UPDATE id REFERENCES funeral(id)"""
-    column_funeral_facilities = cursor.execute(q)
+        sql = """
+            UPDATE funeral_facilities
+            JOIN funeral ON funeral_facilities.장례식장명 = funeral.장례식장명
+            SET funeral_facilities.id = funeral.id
+            """
+        cursor.execute(sql)
+        db.commit()
+        print("업데이트가 성공적으로 수행되었습니다.")
 
-#    q = """SELECT * FROM funeral_info WHERE 시설명 (SELECT 장례식장명 FROM funeral)
-#    ALTER TABLE funeral_info UPDATE id REFERENCES funeral(id)"""
-#    column_funeral_info = cursor.execute(q)
+    except Exception as e:
+        db.rollback()
+        print("업데이트 중 오류가 발생하였습니다:", str(e))
+
+    finally:
+        db.close()
 
     # funeral 테이블의 장례식장명과 funeral_facilites 테이블의 장례식이 일치한다면
     # funeral_facilites 테이블의 아이디 값을 funeral 테이블 기준으로 바꿔서 update query 날리기
+
 if __name__ == '__main__':
     main()
-
 
